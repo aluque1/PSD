92efe24e6644deb7b5cd88d5653fc24a0d8c6820
Nachoski08 <ignsan08@ucm.es>
2023-11-24 03:10:44 +0100

    Otra vez se complica la tarea de debug

---
+++ practica3/practica3_code/lifeGame.sh
@@ -0,0 +1,1 @@
+mpiexec -hostfile machines -np 16 lifeGame 100 100 20 step imagenFinal static
--- practica3/practica3_code/worker.h
+++ practica3/practica3_code/worker.h
@@ -2,6 +2,6 @@
 #include "world.h"
 
 // Enables/Disables the log messages from worker processes
-#define DEBUG_WORKER 0
+#define DEBUG_WORKER 1
 
 
